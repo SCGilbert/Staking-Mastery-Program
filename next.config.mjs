e63c4f3e0b29187e0777995cd5c32cbcfb2d259f
Sam Gilbert <sam.cg147@gmail.com>
2024-12-26 18:16:29 -0500

    Update Next.js configuration: Set output to 'export', enable unoptimized images, and define basePath and assetPrefix for deployment. These changes prepare the application for a more structured deployment environment.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/your-repo-name',
+  assetPrefix: '/your-repo-name',
+};
 
 export default nextConfig;
